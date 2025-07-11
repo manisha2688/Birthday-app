9d0ce03ffb52b0b46cc2cfc7e1aa805566b698e8
Manisha Raman <manisharaman0307@gmail.com>
2025-07-11 19:42:05 +0530

    Initial Commit

---
+++ Birthday-app/Birthday_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_appApp.swift
+//  Birthday-app
+//
+//  Created by Manisha Raman on 11/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthday-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Birthday-app
+//
+//  Created by Manisha Raman on 11/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
